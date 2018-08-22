cca759d5058c992a5d19ce006f54d7902fd1e5fc
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-08-22 11:55:07 +0800

    Update code to compute average per vehicle type correctly

--- MapReduceJobs/Avg_Price_Per_Vehicle_Type.js
+++ MapReduceJobs/Avg_Price_Per_Vehicle_Type.js
@@ -1,6 +1,19 @@
 db.listings.mapReduce(
-    function(){emit(this.type_of_veh, this.price);},
-    function(key, values){return Array.sum(values)/values.length;},
+    function(){
+        emit(this.type_of_veh, this.price);
+    },
+    function(key, values){
+        sum_of_prices = 0;
+        count = 0;
+        for(var itr = 0; itr< values.length; itr++){
+          if(values[itr]){
+            sum_of_prices += values[itr]
+            count += 1
+          }
+        }
+
+        return sum_of_prices/count;
+    },
     {
       out: "price_average_by_vehicle_type"
     }
